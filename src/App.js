1127fc7b240d9826b59a355cae830f124c681024
johnschneider1 <johnlawschneider@gmail.com>
2020-10-08 16:02:16 -0500

    text change

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <main>
       <div className="Container">
-        <h1>CLICK YES to support no violence after our presidential election</h1>
+        <h1>Support the #No Violence movement.  Cast your vote below and unite with Americans celebrating their diversity by raising their voices to say --- No matter the outcome of our Presidential election, let’s stand for No Violence post election.  And pay it forward.</h1>
         <h2>This Many American's support no violence: <h1 className='CountText'>{count}</h1></h2>
 
         <button className="Btn" onClick={() => setCount(count + 1)}>YES</button>
