59f59e7204440f39e03df83fe9b91371e8e969bf
johnschneider1 <johnlawschneider@gmail.com>
2020-10-14 12:25:04 -0500

    changes to text and IP for useeffect

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [newIp, setNewIp] = useState();
 
 
+  // test address http://localhost:5150/api/votes
+  // live address https://counter-nov.herokuapp.com/api/votes
   const handleClick = () => {
     axios
       .post("https://counter-nov.herokuapp.com/api/votes")
@@ -22,6 +25,7 @@ function App() {
     axios
       .get('https://counter-nov.herokuapp.com/api/votes')
       .then((res) => {
+        console.log("test:", res.data)
         setCount(res.data.votes)
       })
       .catch((err) => {
@@ -29,11 +33,35 @@ function App() {
       })
   }, [])
 
+  // useEffect(() => {
+  //   fetch('https://api.ipify.org?format=jsonp?callback=?', {
+  //     method: "GET",
+  //     headers: {},
+  //   })
+  //     .then(res => {
+  //       return res.text()
+  //     }).then(ip => {
+  //       console.log('ip', ip);
+  //       setNewIp(ip)
+  //       axios.post('http://localhost:5150/api/votes', ip)
+  //         .then(res => {
+  //           console.log("ipPostTest:", res.data)
+  //           setNewIp(res.data)
+
+  //         })
+  //         .catch(err => [
+  //           console.error(err)
+  //         ])
+  //     })
+
+  // }, [])
+
   return (
     <main>
       <div className="Container">
         <h1>Support the #No Violence movement.  Cast your vote below and unite with Americans celebrating their diversity by raising their voices to say --- No matter the outcome of our Presidential election, let’s stand for No Violence post election.  And pay it forward.</h1>
-        <h2>This Many American's support no violence: <h1 className='CountText'>{count}</h1></h2>
+        <h2>This Many Americans support no violence: <h1 className='CountText'>{count}</h1></h2>
+        {/* <h3>{newIp}</h3> */}
 
         <button className="Btn" onClick={() => handleClick()}>YES</button>
       </div>
