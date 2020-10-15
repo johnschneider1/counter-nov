8ff20e65d3d1c10fb578a1194b4869a52aaf9622
johnschneider1 <johnlawschneider@gmail.com>
2020-10-15 10:47:31 -0500

    text change on hashtag

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ function App() {
   return (
     <main>
       <div className="Container">
-        <h1>Support the #No Violence movement.  Cast your vote below and unite with Americans celebrating their diversity by raising their voices to say --- No matter the outcome of our Presidential election, let’s stand for No Violence post election.  And pay it forward.</h1>
+        <h1>Support the #noviolence11-4-20 movement.  Cast your vote below and unite with Americans celebrating their diversity by raising their voices to say --- No matter the outcome of our Presidential election, let’s stand for No Violence post election.  And pay it forward.</h1>
         <h2>This Many Americans support no violence: <h1 className='CountText'>{count}</h1></h2>
         {/* <h3>{newIp}</h3> */}
 
